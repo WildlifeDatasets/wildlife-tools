531592abf90e4daebcf914af35ad803a6e702b99
Vojtech  Cermak <cermak.vojtech@seznam.cz>
2023-05-25 11:27:58 +0200

    fix training script

--- train.py
+++ train.py
@@ -2,34 +2,36 @@ import os
 import shutil
 import argparse
 import yaml
-from datetime import datetime
 from parser.experiment import Experiment
+from utils import setup_folder
 
-def setup_folder(output_folder, name=''):
-    timestamp = datetime.now().strftime("%b%d-%H-%M-%S-%f")[:-2]
-    folder = os.path.join(output_folder, timestamp + '_' + name)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    return folder
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True, help="Path to config .yaml file")
-    parser.add_argument("--output", type=str, default="runs", help="Path to output folder")
+    parser.add_argument("--output", type=str, default=None, help="Path to output folder")
     args = parser.parse_args()
+    if args.output is None:
+        folder = setup_folder(args.config)
+    else:
+        folder = args.output
 
-    name, _ = os.path.splitext(os.path.basename(args.config))
-    folder = setup_folder(args.output, name)
+
+    print('Running: ', args.config)
     shutil.copyfile(args.config, os.path.join(folder, os.path.basename(args.config)))
-
     with open(args.config, 'r') as stream:
-        experiment = Experiment(yaml.safe_load(stream))
+        config_dict = yaml.safe_load(stream)
+        experiment = Experiment(config_dict)
 
     for i, datasets in enumerate(experiment.datasets(experiment)):
         trainer = experiment.trainer(experiment, **datasets)
-        trainer.train(**datasets, evaluation=None)
+        if experiment.evaluation:
+            evaluation = experiment.evaluation(datasets=datasets)
+        else:
+            evaluation = None
+        trainer.train(**datasets, evaluation=experiment.evaluation)
 
         folder_split = os.path.join(folder, f"split-{i}")
         if not os.path.exists(folder_split):
            os.makedirs(folder_split)
-        trainer.save(folder_split)
+        trainer.save(datasets=datasets, folder=folder_split)
