8408fe3513bef6a0b81a3d630124c5c1f77fe6df
Vojtech  Cermak <cermak.vojtech@seznam.cz>
2023-11-13 12:59:58 +0100

    cleanup

--- utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from datetime import datetime
-
-def setup_folder(config_filename, root='runs', name=''):
-    name, _ = os.path.splitext(os.path.basename(config_filename))
-    timestamp = datetime.now().strftime("%b%d-%H-%M-%S-%f")[:-2]
-    folder = os.path.join(root, name + '_' + timestamp)
-    os.makedirs(folder, exist_ok=True)
-    return folder
