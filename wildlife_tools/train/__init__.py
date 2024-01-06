d781ec2a716c5fa891c24ee34d0d904950fd2850
Lukas Picek <lukaspicek@gmail.com>
2024-01-05 17:06:03 -1000

    chore:resolving code quality issues

--- wildlife_tools/train/__init__.py
+++ wildlife_tools/train/__init__.py
@@ -2,4 +2,5 @@ from .backbone import TimmBackbone
 from .callbacks import EpochCallbacks
 from .objective import ArcFaceLoss, SoftmaxLoss, TripletLoss
 from .optim import OptimizerAdam, OptimizerSGD
-from .trainer import BasicTrainer, ClassifierTrainer, EmbeddingTrainer, set_seed
+from .trainer import (BasicTrainer, ClassifierTrainer, EmbeddingTrainer,
+                      set_seed)
