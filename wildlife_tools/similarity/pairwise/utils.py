bc8b2b3361dbbd4a93bb995e75ac1d8e9646758e
Vojtech Cermak <37118002+VojtechCermak@users.noreply.github.com>
2024-11-27 12:17:02 +0100

    version 1.0.2 (#16)
    * change loading behaviour when annotation is None
    
    * feature extractor puts model back to cpu
    
    * add parameter for silent tqdm
    
    * fix nan in bbox and segmetnation
    
    * Loading bboxes fixed
    
    * Segmentation can be polygon
    
    * Added mask_crop to image loading
    
    * improve loading methods
    
    * Support for loading uncompressed RLE
    
    * Merge readme (#12)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Readme updated
    
    ---------
    
    Co-authored-by: sadda <lukas.adam.cr@gmail.com>
    
    * Wildfusion (#14)
    
    * add refactored nn classifiers
    
    * refactor pairwise matching similarity
    
    * add wildfusion
    
    * delete optim
    
    * merge from origin
    
    * fix imports
    
    * update docs
    
    * cleanup
    
    * cleanup
    
    * chore: formatting
    
    * chore: change naming
    
    * chore: formatting
    
    * chore: black formatting
    
    * chore: formatting isort
    
    * add visualisation tools
    
    * fix: examples consistency
    
    * examples: update
    
    * docs: fix imports in examples
    
    * chore: formatting
    
    * chore: update readme
    
    * chore: update readme
    
    * Update README.md
    
    * refactor: move visualisation function to utils and clean up imports
    
    * chore: update examples
    
    * chore: formatting
    
    ---------
    
    Co-authored-by: sadda <lukas.adam.cr@gmail.com>

---
+++ wildlife_tools/similarity/pairwise/utils.py
@@ -0,0 +1,44 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+def visualise_matches(img0: Image, keypoints0: np.ndarray, img1: Image, keypoints1: list, ax=None):
+    """
+    Visualise matches between two images.
+
+    Args:
+        img0 (np.array or PIL Image): First image.
+        keypoints0 (np.array): Keypoints in the first image.
+        img1 (np.array): Second image.
+        keypoints1 (np.array): Keypoints in the second image.
+        ax (matplotlib.axes.Axes, optional): Matplotlib axis to draw on. If None, a new axis is created.
+    """
+
+    # Convert images to numpy arrays
+    img0 = np.array(img0)
+    img1 = np.array(img1)
+
+    keypoints0 = [cv2.KeyPoint(int(x[0]), int(x[1]), 1) for x in keypoints0]
+    keypoints1 = [cv2.KeyPoint(int(x[0]), int(x[1]), 1) for x in keypoints1]
+
+    # Create dummy matches (DMatch objects)
+    matches = [cv2.DMatch(i, i, 0) for i in range(len(keypoints0))]
+
+    # Draw matches
+    img_matches = cv2.drawMatches(
+        img0,
+        keypoints0,
+        img1,
+        keypoints1,
+        matches,
+        None,
+        flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+    )
+
+    # Plotting
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.imshow(img_matches)
+    ax.axis("off")
