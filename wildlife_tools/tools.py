fe67b33ae0dd4da14bb87f828d7af74b6b3f0dfd
sadda <lukas.adam.cr@gmail.com>
2025-10-13 11:45:20 +0000

    Removed duplicate function

--- wildlife_tools/tools.py
+++ wildlife_tools/tools.py
@@ -8,17 +8,6 @@ import torch.backends.cudnn
 from PIL import Image
 
 
-def set_seed(seed=0, device="cuda"):
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if device == "cuda":
-        torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
-
 def frame_image(img, frame_width, color=(255, 0, 0)):
     b = frame_width
     ny, nx = img.shape[0], img.shape[1]
